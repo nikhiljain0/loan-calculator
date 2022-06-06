b8008cd1c84c53294843fddbe2c1c040a6317041
Nikhil Jain <nikhil.jain@nikhilj-ltmvpgx.internal.salesforce.com>
2022-06-06 21:19:36 +0530

    Fixes

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const loanjs = require('loanjs')
 
-var loan = new loanjs.Loan(
-    1000, // amount
-    12,   // installments number
-    5,    // interest rate
-    true  // diminishing
-  )
+var http = require('http');
+var fs = require('fs');
 
-  console.log(loan)
+const PORT=8080; 
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
+
+// var loan = new loanjs.Loan(
+//     1000, // amount
+//     12,   // installments number
+//     5,    // interest rate
+//     true  // diminishing
+//   )
+
+//   console.log(loan)
